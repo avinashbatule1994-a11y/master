0ce2a34fcf70a4a7397d11520f693403433dcc83
Avinash Batule <avinash.batule1994@gmail.com>
2026-01-01 18:24:29 +0530

    feat: show open/closed status, menu, and gallery for students

--- studentHub/src/app/features/student/listing-details/listing-details.component.ts
+++ studentHub/src/app/features/student/listing-details/listing-details.component.ts
@@ -6,10 +6,44 @@ import { ActivatedRoute } from '@angular/router';
     styleUrls:['./listing-details.component.scss']
 })
 export class ListingDetailsComponent {
+
   listingId!: number;
+
+  listing: any;
+
+  dummyListings = [
+    {
+      id: 1,
+      name: 'Sai Mess',
+      type: 'mess',
+      city: 'Pune',
+      area: 'Hadapsar',
+      active: true,
+      images: [
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150'
+      ],
+      menu: {
+        enabled: true,
+        breakfast: 'Poha',
+        lunch: 'Thali',
+        dinner: 'Rice & Dal'
+      }
+    },
+    {
+      id: 2,
+      name: 'Shiv Hostel',
+      type: 'hostel',
+      city: 'Pune',
+      area: 'Wakad',
+      active: false,
+      images: ['https://via.placeholder.com/150']
+    }
+  ];
 
   constructor(private route: ActivatedRoute) {
     this.listingId = Number(this.route.snapshot.paramMap.get('id'));
+    this.listing = this.dummyListings.find(l => l.id === this.listingId);
   }
   addToFavorites() {
   const favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
